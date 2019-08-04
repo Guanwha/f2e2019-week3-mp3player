9c65553d7b73f8c348f57e04c479a976831ef631
Eric Fei <guanwha@gmail.com>
2019-08-04 10:05:53 +0800

    Fixed the issue of "Absolute imports should come before relative imports"

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+// scrollview
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
+import { RecycleScroller } from 'vue-virtual-scroller';
+
+// common
 import Vue from 'vue';
 import App from './App';
 import store from './stores';
 import './styles/_reset.css';
 import './styles/_variables.scss';
 import './styles/_normalize.scss';
-
-// scrollview
-import { RecycleScroller } from 'vue-virtual-scroller';
-import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
 
 Vue.config.productionTip = false;
 Vue.component('RecycleScroller', RecycleScroller);
